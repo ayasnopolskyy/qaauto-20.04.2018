681bb2ff1e6950e514e691c6b8b2f09a8cee8ee1
ayasnopolskyy <andrey.yasnopolskyy@gmail.com>
2018-05-04 16:22:30 +0300

    GoodCodeExample

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -19,12 +19,15 @@ public class LinkedinLoginTest {
                 "LinkedIn: Log In or Sign Up", "Wrong Result");
 
         WebElement userEmailField = webDriver.findElement(By.xpath("//input[@id='login-email']"));
-        userEmailField.sendKeys("" );
+        userEmailField.sendKeys("gor1362@gmail.com" );
 
         WebElement userPasswordField = webDriver.findElement(By.xpath("//input[@id='login-password']"));
-        userPasswordField.sendKeys("" );
+        userPasswordField.sendKeys("p0o9P)O(" );
 
         WebElement signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
         signInButton.sendKeys(Keys.RETURN);
+
+        Assert.assertEquals(webDriver.getTitle(),
+                "get set up to advance your career in 3 easy steps", "Wrong Result");
     }
 }
